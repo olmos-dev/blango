b2439b0740cab230f0adf3d4a6939e61d6a87683
Alberto Olmos <albrtho94@gmail.com>
2026-07-17 23:41:11 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -4,9 +4,11 @@ from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
 from blog.models import Post
+import logging
 
 register = template.Library()
 user_model = get_user_model()
+logger = logging.getLogger(__name__)
 
 @register.filter
 def author_details(author, current_user):
@@ -34,6 +36,7 @@ def author_details(author, current_user):
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 @register.simple_tag
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect 
 from django.utils import timezone
 from blog.models import Post
-from django.shortcuts import render, get_object_or_404
-from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_cookie
 
-
-
-
-# Create your views here.
 logger = logging.getLogger(__name__)
 
+# Create your views here.
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     logger.debug("Got %d posts", len(posts))
