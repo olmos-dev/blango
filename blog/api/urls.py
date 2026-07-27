c6e4a4d80343e10446eb43ae236f4c29ee1c169e
Alberto Olmos <albrtho94@gmail.com>
2026-07-27 15:09:52 -0600

    Finish viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,12 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+from blog.api.views import UserDetail, PostViewSet,TagViewSet
 
-from blog.api.views import PostList, PostDetail, UserDetail
+router = DefaultRouter()
+router.register("posts", PostViewSet)
+router.register("tags", TagViewSet)
+
 
 urlpatterns = [
-    path("posts/", PostList.as_view(), name="api_post_list"),
-    path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
+    path("", include(router.urls)),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+#urlpatterns = format_suffix_patterns(urlpatterns) 
